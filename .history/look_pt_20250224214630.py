d10dd8a2724abfe4f3e7b2b419f7096f8f8bb8b6
qizhihuanying <qzhycloud@outlook.com>
2025-02-24 22:15:26 +0800

    test

--- .history/look_pt_20250224214630.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-file_path = "./project/models/binary_head/binary_head_full.pt"
-state_dict = torch.load(file_path, map_location="cpu")
-
-for key, value in state_dict.items():
-    print(f"{key}: {value}")
